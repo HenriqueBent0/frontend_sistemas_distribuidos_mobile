9f94ec7d8addc3f9eca3343f9440b8c670b5fb29
JonnyLynch8 <jonnylynch8@gmail.com>
2025-10-21 15:27:34 -0300

    Merge origin/main into main
    # Conflicts:
    #	README.md

--- src/main/java/frontend/TelaBalanco.java
+++ src/main/java/frontend/TelaBalanco.java
@@ -2,8 +2,18 @@
 package frontend;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.rmi.Naming;
+import java.util.List;
+import model.Produto;
+import remote.EstoqueServico;
 
 public class TelaBalanco extends JFrame{
+    private EstoqueServicos api;
+    private Jtable tabela;
+    private DefaultTableModel modeloTabela;
+    private JLabel lblTotalEstoque
     
     public TelaBalanco() {
         setTitle("📊 Balanço Físico/Financeiro");
