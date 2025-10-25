e3495b016609e8ae14cf2d21543056e8254336fd
IanAkira <ian.fujimori09@gmail.com>
2025-10-25 16:55:25 -0300

    feat: integração da classe "TelaListaPrecos" com o serviço remoto "EstoqueServico"

--- src/main/java/frontend/TelaListaPrecos.java
+++ src/main/java/frontend/TelaListaPrecos.java
@@ -1,14 +1,31 @@
-
 package frontend;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.rmi.Naming;
+import java.util.List;
+import model.Produto;
+import remote.EstoqueServico;
 
 
 public class TelaListaPrecos extends JFrame {
 
+    private EstoqueServico api;
 
     public TelaListaPrecos() {
         setTitle("Lista de Preços");
         setSize(600, 400);
         setLocationRelativeTo(null);
 
+        try {
+            api = (EstoqueServico) Naming.lookup("rmi://localhost:1099/EstoqueService");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(this, "Erro ao conectar no servidor: " + e.getMessage());
+            dispose();
+            return;
+        }
 
     }
+    
+}
