afec02b5a80946dd6ef14c6fdbf886b0d7b9e4d0
pedrokrukz <ppok306@gmail.com>
2025-11-03 18:06:12 -0300

    feat: conectar ao EstoqueService via RMI e tratar erros de conexão

--- src/main/java/frontend/TelaProdutosCriticos.java
+++ src/main/java/frontend/TelaProdutosCriticos.java
@@ -2,6 +2,7 @@ package frontend;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.awt.*;
 import java.rmi.Naming;
 import remote.EstoqueServico;
 
@@ -23,3 +24,18 @@ public class TelaProdutosCriticos extends JFrame {
             dispose();
             return;
         }
+
+        modeloTabela = new DefaultTableModel(new String[]{
+                "Produto", "Quantidade Mínima", "Quantidade em Estoque"
+        }, 0) {
+            @Override public boolean isCellEditable(int row, int column) { return false; }
+        };
+
+        tabela = new JTable(modeloTabela);
+        tabela.setFillsViewportHeight(true);
+
+        add(new JScrollPane(tabela), BorderLayout.CENTER);
+
+        carregarProdutosCriticos();
+    }
+
