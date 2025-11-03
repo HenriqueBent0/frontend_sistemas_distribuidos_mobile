4c5cab0f826314c35f0d9571671d60f108d0f5b5
pedrokrukz <ppok306@gmail.com>
2025-11-03 18:04:35 -0300

    feat: criação da classe TelaProdutosCriticos

---
+++ src/main/java/frontend/TelaProdutosCriticos.java
@@ -0,0 +1,25 @@
+package frontend;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.rmi.Naming;
+import remote.EstoqueServico;
+
+public class TelaProdutosCriticos extends JFrame {
+
+    private EstoqueServico api;
+    private JTable tabela;
+    private DefaultTableModel modeloTabela;
+
+    public TelaProdutosCriticos() {
+        setTitle("⚠ Produtos Abaixo do Mínimo");
+        setSize(600, 400);
+        setLocationRelativeTo(null);
+
+        try {
+            api = (EstoqueServico) Naming.lookup("rmi://localhost:1099/EstoqueService");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(this, "Erro ao conectar no servidor: " + e.getMessage());
+            dispose();
+            return;
+        }
